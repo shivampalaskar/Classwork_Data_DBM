831a33fde2261d00b015ee35ed2aec2e4a8d3df2
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-03 17:30:59 +0530

    Understand Reference Variable

--- Cpp/Understand_ReferenceVariable.cpp
+++ Cpp/Understand_ReferenceVariable.cpp
@@ -27,36 +27,40 @@
 */
 
 
-
 #include <iostream>
 using namespace std;
 
-class Test
-{
-private :
-	int publicVar1 = 1;
-	int publicVar2 = 2;
-public :
-	void function(int,int);
-	void function1(int,int);
-};
-
-void Test :: function(int i,int j){
-	publicVar1 = i;
-	publicVar2 = j;
-}
-
-void Test :: function1(int publicVar1,int publicVar2){
-	this->publicVar1 = publicVar1;
-	this->publicVar2 = publicVar2;
-}
+void swap_by_address(int*,int*);
+void swap_by_reference(int&,int&);
 
 int main() {
-	Test t;
-	int num;
-	double d;
-	int &ref1 = num; // Read as : "ref1 is an integer reference initialized to num"
-	double &ref2 = d;  //Read as : "ref2 is a double reference initialized to d"
+	int num1 = 5;
+	int num2 = 10;
+	cout<<"Before swap_by_address : "<<endl;
+	cout<<"num1 : "<<num1<<endl<<"num2 : "<<num2<<endl;
+	swap_by_address(&num1, &num2);
+	cout << "After swap_by_address : " << endl;
+	cout << "num1 : " << num1 << endl << "num2 : " << num2 << endl;
+	cout<<endl;
+	cout << "Before swap_by_reference : " << endl;
+	cout << "num1 : " << num1 << endl << "num2 : " << num2 << endl;
+	swap_by_reference(num1,num2);
+	cout << "After swap_by_reference : " << endl;
+	cout << "num1 : " << num1 << endl << "num2 : " << num2 << endl;
 
 	return 0;
+}
+
+void swap_by_address(int *ptrNum1,int *ptrNum2){
+	int temp;
+	temp = *ptrNum1;
+	*ptrNum1 = *ptrNum2;
+	*ptrNum2 = temp;
+}
+
+void swap_by_reference(int &refNum1,int &refNum2){
+	int temp;
+	temp = refNum1;
+	refNum1 = refNum2;
+	refNum2 = temp;
 }

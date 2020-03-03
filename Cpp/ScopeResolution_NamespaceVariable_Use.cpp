a32a639f061614391b31665b2a13ddb29103946e
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-03 18:10:06 +0530

    Update ScopeResolution_NamespaceVariable_Use.cpp

--- Cpp/ScopeResolution_NamespaceVariable_Use.cpp
+++ Cpp/ScopeResolution_NamespaceVariable_Use.cpp
@@ -15,6 +15,7 @@ int num1 = 100;
 namespace ns1{
 int num1 = 1;
 }
+
 namespace ns2{
 int num1 = 2;
 }
@@ -22,11 +23,12 @@ int num1 = 2;
 int main() {
 	int num1 = 30;
 	cout<<"Local Variable num1 : "<<num1<<endl;
+
 	cout<<"Global Variable num1 : "<<::num1<<endl;
+
 	cout<<"ns1 Variable num1 : "<<ns1::num1<<endl;
 	cout<<"ns2 Variable num1 : "<<ns2::num1<<endl;
 
-	//Or
 	using namespace ns1;
 	cout<<"ns1 Variable num1 : "<<num1<<endl;
 	using namespace ns2;

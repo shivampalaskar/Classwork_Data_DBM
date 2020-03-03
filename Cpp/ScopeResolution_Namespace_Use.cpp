3a06651bc28b79d9b61dbfd9d84347dd10bbf12b
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-03 15:53:42 +0530

    Create ScopeResolution_Namespace_Use.cpp
    Scope resolution Operator used for namespace

---
+++ Cpp/ScopeResolution_Namespace_Use.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Shivam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Scope resolution Operator used for namespace
+//============================================================================
+
+#include <iostream>
+//using namespace std
+
+int main() {
+	std::cout<<"Test";
+	return 0;
+}

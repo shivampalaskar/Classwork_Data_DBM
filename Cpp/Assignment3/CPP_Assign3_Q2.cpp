8b4f14e6bf94d934464393f7d8bd4844522455e9
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-05 17:36:49 +0530

    Create CPP_Assign3_Q2.cpp

---
+++ Cpp/Assignment3/CPP_Assign3_Q2.cpp
@@ -0,0 +1,90 @@
+//============================================================================
+// Name        : CPP_Assignment.cpp
+// Author      : SHIVAM PALASKAR
+// Version     :
+// Copyright   : Your copyright notice
+// Description : CPP_Assign3_Q1
+//============================================================================
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+class Matrix{
+private :
+	int** mtrx1;
+	int** mtrx2;
+public :
+	Matrix();
+	Matrix(int,int);
+	void add(Matrix &,Matrix &);
+	void printMatrix();
+};
+
+void Matrix::printMatrix() {
+	int i,j;
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+			cout << mtrx1[i][j] << '\t';
+		}
+	}
+}
+
+Matrix::Matrix() {
+	mtrx1 = new int*[2];
+	for (int i = 0; i < 2; ++i)
+		mtrx1[i] = new int[2];
+
+	mtrx2 = new int*[2];
+	for (int i = 0; i < 2; ++i)
+		mtrx2[i] = new int[2];
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			mtrx1[i][j] = i+j;
+			mtrx1[i][j] = i+j;
+		}
+	}
+}
+
+Matrix::Matrix(int row,int col) {
+	mtrx1 = new int*[row];
+	for (int i = 0; i < col; ++i)
+		mtrx1[i] = new int[row];
+
+	mtrx2 = new int*[col];
+	for (int i = 0; i < col; ++i)
+		mtrx2[i] = new int[col];
+
+}
+
+void Matrix::add(Matrix &arr1, Matrix &arr2) {
+	int sum[2][2];
+	int i;
+	int j;
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+
+			sum[i][j] = arr1.mtrx1[i][j] + arr2.mtrx2[i][j];
+		}
+	}
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+			cout << sum[i][j] << '\t';
+
+		}
+		cout << endl;
+	}
+
+}
+
+int main() {
+	Matrix m1;
+	Matrix m2;
+	//m1.printMatrix();
+
+	cout<<"Matrix addition is :"<<endl;
+		m1.add(m1,m2);
+
+	return 0;
+}
